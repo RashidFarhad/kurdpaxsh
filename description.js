edf3fcf7e82f455988cd2acc63d327e2270fc4c0
Sizar Azad <sizarazad86@gmail.com>
2020-06-09 12:25:35 +0300

    update

--- description.js
+++ description.js
@@ -27,7 +27,7 @@ function loadPackageInfo() {
 	var form_url = current_url_path + "/packageInfo/" + urlSelfParts[1];
 	if (navigator.userAgent.search(/Cydia/) == -1) {
 		$("#showAddRepo_").show();
-		$("#open_cydia").attr("href", "cydia://url/https://cydia.saurik.com/api/share#?source=https://repo.sizu.gq/&package=" + urlSelfParts[1]);
+		$("#open_cydia").attr("href", "cydia://url/https://cydia.saurik.com/api/share#?source=https://kurdpaxsh.ml/&package=" + urlSelfParts[1]);
 	}
 	$.ajax({
 		url: form_url,
